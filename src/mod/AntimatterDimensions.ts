e2e6c5c5887d2de82b6c40f1cfc9669d4540db28
QwQe308 <78683793+QwQe308@users.noreply.github.com>
2024-07-26 10:54:46 +0800

    huh

--- src/mod/AntimatterDimensions.ts
+++ src/mod/AntimatterDimensions.ts
@@ -2,13 +2,13 @@ import { MR2Globals } from "magic-research-2-modding-sdk";
 export function ADmod(MR2: MR2Globals) {
   //what
   //disable all channeling spells
-  const allSpells = MR2.Spells.getAll();
-  const channelingSpells = allSpells.filter(
-    (spell) => spell instanceof MR2.BasicChannelingSpellBase,
-  );
-  for (let spell of channelingSpells) {
-    MR2.Spells.delete(spell.getId());
-    MR2.Actions.delete(spell.getId());
-  }
+  //const allSpells = MR2.Spells.getAll();
+  //const channelingSpells = allSpells.filter(
+  //  (spell) => spell instanceof MR2.BasicChannelingSpellBase,
+  //);
+  //for (let spell of channelingSpells) {
+  //  MR2.Spells.delete(spell.getId());
+  //  MR2.Actions.delete(spell.getId());
+  //}
   //wow so channeling is disabled, that's fantastic(what)
 }
